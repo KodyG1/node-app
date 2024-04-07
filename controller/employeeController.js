9a953d99007990c71a9c53f16032f1091fc27fea
KodyG1 <Kgalvanpec16@gmail.com>
2024-04-07 18:46:54 -0500

    Delete is working but commented out

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,18 +1,23 @@
 import Employee from "../model/employeeModel.js"
+
 export const create = async(req, res) => {
     try {
         const employeeData = new Employee(req.body)
         const { userName } = employeeData
         const employeeExist = await Employee.findOne({userName})
+      
         if(employeeExist) {
             return res.status(200).json({message: "Employee already exists."})
         }
+      
         const savedEmployee = await employeeData.save();
         res.status(200).json(savedEmployee)
+    
     } catch (error) {
         res.status(500).json({error: "Internal Server Error"})
     }
 }
+
 export const fetch = async(req, res) => {
     try {
         //res.json("Hello World")
@@ -20,6 +25,7 @@ export const fetch = async(req, res) => {
         if(employees.length === 0) {
             return res.status(404).json({message: "Employees not found!"})
         }
+   
         res.status(200).json(employees)
     } catch (error) {
         res.status(500).json({error: "Internal Server Error"})
@@ -41,3 +47,19 @@ export const update = async(req, res) => {
         res.status(500).json({error: "Internal Server Errror"})
     }
 }
+
+//  export const deleteEmployee = async(req, res) => {
+//      try {
+//          const id = req.params.id
+//          const employeeExist = await Employee.findOne({_id:id})
+
+//          if(!employeeExist) {
+//              return res.status(404).json({message: "Employee not found"})
+//          }
+
+//          await Employee.findByIdAndDelete(id)
+//          res.status(201).json({message: "Employee terminated!!"})
+//      } catch (error) {
+//          res.status(500).json({error: "Internal Server Error"})
+//      }
+//  }
